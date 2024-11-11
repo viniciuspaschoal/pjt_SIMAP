de848d418cc5efcbbd00cc720689ebe4cbd49419
viniciuspaschoal <viniciuspaschoal1847@gmail.com>
2024-11-10 23:56:12 -0300

    estados para abrir e fechar o menu quando estiverem 'open' e 'close'

--- src/components/NavegationsComands.jsx
+++ src/components/NavegationsComands.jsx
@@ -34,7 +34,76 @@ function NavComands() {
 
             </div>
 
-            
+            {estadoMenu === 'close' && (
+                <nav class="menu-lateral">
+
+                    <div class="container-icons">
+
+                        <i class="fa-solid fa-house house"></i>
+                        <div class="separador-dois"></div>
+
+                        <i class="fa-solid fa-magnifying-glass lupa"></i>
+                        <div class="separador-dois"></div>
+
+                        <div class="escrita-filter">
+                            <i class="fa-solid fa-sliders filtro_btn"></i>
+                        </div>
+
+                        <div class="confing-buttons">
+                            <div class="duvidas">
+                                <p class="interrogacao">?</p>
+                            </div>
+                            <i class="fa-solid fa-gear"></i>
+                        </div>
+
+                    </div>
+                </nav>
+
+            )}
+
+            {estadoMenu === 'open' && (
+
+                <nav class="menu-lateral-open">
+
+                    <div class="container-icons">
+                        <div class="escrita-open-house">
+                            <i class="fa-solid fa-house house"></i>
+                            <p class="escrita-open">Início</p>
+                        </div>
+
+                        <div class="separador-tres"></div>
+
+                        <div class="escrita-open-lupa">
+                            <i class="fa-solid fa-magnifying-glass lupa"></i>
+                            <p class="escrita-open">Pesquisar</p>
+                        </div>
+
+                        <div class="separador-tres"></div>
+
+                        <div class="escrita-open-filter">
+                            <i class="fa-solid fa-sliders filtro_btn"></i>
+                            <p class="escrita-open">Filtrar</p>
+                        </div>
+
+                        <div class="confing-buttons">
+                            <div class="div-duvidas">
+                                <div class="duvidas">
+                                    <p class="interrogacao">?</p>
+                                </div>
+                                <p class="guia">Guia</p>
+                            </div>
+
+                            <div class="escrita-open-config">
+                                <i class="fa-solid fa-gear"></i>
+                                <p class="escrita-open">Filtrar</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </nav>
+
+            )}
 
 
 
