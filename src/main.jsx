c2f4a212dc1f41bdce3555e034be39eb170099dc
viniciuspaschoal <viniciuspaschoal1847@gmail.com>
2024-12-06 00:17:52 -0300

    Desenvolvida tela de login

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Container from './Container.jsx'
+import Login from './Login.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Container />
+    <Login />
   </StrictMode>,
 )
