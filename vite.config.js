6432e2a48e4717bc86ddebef45ed75220961e5c4
viniciuspaschoal <viniciuspaschoal1847@gmail.com>
2025-09-24 00:10:24 -0300

    Configurações iniciais

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'  // Adicionando a importação do tailwindcss
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
+    tailwindcss(),  // Agora o tailwindcss é passado como plugin corretamente
   ],
 })
