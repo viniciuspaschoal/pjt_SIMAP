819d41104d2b2b1291dcb08a90fdf79ffdeaadd4
viniciuspaschoal <viniciuspaschoal1847@gmail.com>
2025-11-12 21:11:34 -0300

    Adicionando EndPoint chamada para POST dos filtros

--- src/services/alunoService.js
+++ src/services/alunoService.js
@@ -1,3 +1,8 @@
 import api from "./api";
 
 export const getAlunoByRa = (ra) => api.get(`/aluno/ra/${ra}`)
+
+export const aplicarFiltros = (filtrosSelecionados) => {
+  console.log("➡️ Enviando filtros para backend:", filtrosSelecionados);
+  return api.post('/aluno/filtro', filtrosSelecionados);
+}
